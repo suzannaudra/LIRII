892309a09f1e113b2899d543734514d355ca2ca2
Suzann Kowalski <suzannaudra@gmail.com>
2020-04-01 17:18:11 -0700

    concert this function working and do what it says function working

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# LIRI
+This bot will take a band name and find a list of upcoming shows or events.  Given the COV19 many of the shows shown will probably be cancelled. To find a show use concert-this.  This bot will also give you song information through spotify. Use spotify-this to find the song and more information. 
  
+concert-this
+node liri.js concert-this <artist/band name>
+
+This will search the Bands in Town Artist Events API for an artist and show the following information about each event in the terminal and to the log.txt file:
+
+  * Name of the venue
+  * Venue location
+  * Date of the Event 
+
+spotify-this
+node liri.js spotify-this '<song name>'
+
+This will search the Spotify API for a song and show the following information about the song in the terminal and to the log.txt file:
+
+  * Artist(s)
+  * The song's name
+  * A preview link of the song from Spotify
+  * The album that the song is from
+
+
+Do-What-it-says
+
+This will return a default of the command in the random.txt file. 
+The spotify Keys are here:
--- liri.js
+++ liri.js
@@ -1,41 +1,112 @@
 require("dotenv").config();
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
+var fs = require("fs");
+var request = require("request")
+
 
 
 
 var spotify = new Spotify(
   keys.spotify
  );
- 
-   spotify
- .search({ type: 'track', query: songName || 'The Sign', limit: 1 })
- .then(function(response) {
-   console.log(response.tracks.items);
-   console.log("resultsreturned")
- })
- .catch(function(err) {
-   console.log("err");
- });
+
+
+
+
+ function music (param){ 
+   console.log(param)
+  if (param === '') {
+    param = "The Sign";
+  }
+  spotify
+  .search({ type: 'track', query: param, limit: 1 })
+  .then(function(response) {
+    console.log(response.tracks.items);
+    console.log("resultsreturned")
+    var tableArray = [];
+
+    
+    for (var i = 0; i < response.tracks.items.length; i++ ) {
+        var result = {
+            artist : response.tracks.items[i].album.artists[0].name,
+            album_name : response.tracks.items[i].album.name,
+            song_name : response.tracks.items[i].name,
+            preview_url : response.tracks.items[i].preview_url 
+        }
+        tableArray.push(result);
+        console.table(tableArray);
+      }
+  })
+  .catch(function(err) {
+    console.log("err");
+  });
+
+ }
+  
+   
 console.log(process.argv)
 
 var command = process.argv[2]
-var songName = process.argv.slice(3).join(' ')
+var param = process.argv.slice(3).join(' ')
 console.log (command)
-console.log (songName)
+console.log (param)
 
-if (songName == undefined) {
-  songName = " ";
-  var tableArray = [];
+function runAction(command, param) {
+  switch (command) {
+      case "concert-this":
+          concert(param)
+          break
+      case "spotify-this":
+          music(param)
+          break
+      case "do-what-it-says":
+          doWhatItSays()
+          break 
+      default:
+      ("That is not a command that I recognize, please try again.") 
+  }
+}
+runAction(command,param)
 
-  for (var i = 0; i < data.tracks.items.length; i++ ) {
-      var result = {
-          artist : data.tracks.items[i].album.artists[0].name,
-          album_name : data.tracks.items[i].album.name,
-          song_name : data.tracks.items[i].name,
-          preview_url : data.tracks.items[i].preview_url 
-      }
-      tableArray.push(result);
-      tableArray.printInfo(random.txt);
-  }}
+ 
 
+
+  
+
+  function concert(artist){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist.replace(" ", "+") + "/events?app_id=codingbootcamp" 
+    
+   
+    request(queryUrl, function(err, response, body){
+        // If the request is successful
+        if (!err && response.statusCode === 200) {
+            // Save parsed body in a new variable for easier use
+            var concertInfo = JSON.parse(body)
+            console.log('DATA', concertInfo)
+            console.log(artist + " concert information:")
+
+            for (i=0; i < concertInfo.length; i++) {
+                
+                region = concertInfo[i].venue.region
+                if (region === "") {
+                    region = concertInfo[i].venue.country
+                }
+                
+                console.log("Venue: " + concertInfo[i].venue.name)
+                console.log("Location: " + concertInfo[i].venue.city + ", " + region);
+                console.log("Date: " + concertInfo[i].datetime);
+            }
+        }
+    })
+  }
+      function doWhatItSays() {
+      fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+  
+        var dataArr = data.split(',')
+  
+        runAction(dataArr[0],dataArr[1])
+      });
+    }
